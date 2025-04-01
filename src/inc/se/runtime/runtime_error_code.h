27f11ff3fc46fa92ce225b28cb442cf3b4f3172f
Egor Tsyganchuk <cyaneor@gmail.com>
2025-04-01 22:54:10 +0300

    runtime_error.h переименован в runtime_error_code.h

--- src/inc/se/runtime/runtime_error_code.h
+++ src/inc/se/runtime/runtime_error_code.h
@@ -3,23 +3,24 @@
  * @brief Содержит объявление переменной кода ошибки времени выполнения
  *        с локальным хранением для потоков.
  *
- * Этот заголовочный файл предоставляет объявление переменной с локальным хранением для потоков,
- * используемой для хранения кодов ошибок времени выполнения.
+ * Этот заголовочный файл предоставляет объявление переменной
+ * с локальным хранением для потоков, используемой
+ * для хранения кодов ошибок времени выполнения.
  */
 
-#ifndef SE_RUNTIME_ERROR_H
-#define SE_RUNTIME_ERROR_H
+#ifndef SE_RUNTIME_ERROR_CODE_H
+#define SE_RUNTIME_ERROR_CODE_H
 
 #include <se/error/error_code.h>
 #include <se/attribute/attribute.h>
 
 /**
- * @var se_error_code_t se_runtime_error
+ * @var se_error_code_t se_runtime_error_code
  * @brief Переменная с локальным хранением для потоков,
  *        содержащая текущий код ошибки времени выполнения.
  */
 SE_COMPILER_EXTERN
 SE_ATTRIBUTE(THREAD_LOCAL)
-se_error_code_t se_runtime_error;
+se_error_code_t se_runtime_error_code;
 
-#endif // SE_RUNTIME_ERROR_H
+#endif // SE_RUNTIME_ERROR_CODE_H
--- src/src/se/runtime/runtime_error.c
+++
@@ -1,4 +0,0 @@
-#include <se/runtime/runtime_error.h>
-
-SE_ATTRIBUTE(THREAD_LOCAL)
-se_error_code_t se_runtime_error = SE_ERROR_CODE_NONE;
